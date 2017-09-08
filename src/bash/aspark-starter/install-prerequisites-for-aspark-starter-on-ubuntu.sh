e2c97b97b6c01417fd6c4f8fc31ad6e7c25bef7c
FirstName LastName <first_name.last_name@gmail.com>
2017-09-08 14:16:10 +0300

    stop v0.0.1 - add the install prereqs script

--- src/bash/aspark-starter/install-prerequisites-for-aspark-starter-on-ubuntu.sh
+++ src/bash/aspark-starter/install-prerequisites-for-aspark-starter-on-ubuntu.sh
@@ -4,10 +4,22 @@
 
 set -eu -o pipefail # fail on error , debug all lines
 
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo priveledge to run this script"
+
+echo installing the must-have pre-requisites
 sudo apt-get install -y perl
 sudo apt-get install -y zip unzip
 sudo apt-get install -y exuberant-ctags
 sudo apt-get install -y mutt
 
+echo installing the nice-to-have pre-requisites
+echo you have 5 seconds to proceed ...
+echo or 
+echo hit Ctrl+C to quit
+echo -e "\n"
+sleep 6
+
+sudo apt-get install -y tig
 
 # eof file src/bash/aspark-starter/install-prerequisites-for-aspark-starter.sh
